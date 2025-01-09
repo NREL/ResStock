cd746c33fbf1748e00888cc9b244d90fd72df60f
Yingli <ylou2@nrel.gov>
2025-01-09 15:11:13 -0700

    measure testing

--- measures/ResStockArgumentsPostHPXML/tests/test_measure.rb
+++ measures/ResStockArgumentsPostHPXML/tests/test_measure.rb
@@ -2,39 +2,30 @@
 
 require 'openstudio'
 require_relative '../../../resources/hpxml-measures/HPXMLtoOpenStudio/resources/minitest_helper'
-require_relative '../../../resources/hpxml-measures/HPXMLtoOpenStudio/resources/hpxml'
 require_relative '../measure.rb'
 
 class ResStockArgumentsPostHPXMLTest < Minitest::Test
   def test_load_flexibility_measure
-    osw_file = '../resources/load_flex_post_hpxml_upgrade.osw'
+    osw_file = 'SFD_1story_FB_UA_GRG_MSHP_FuelTanklessWH_upgrade.osw'
     puts "\nTesting #{File.basename(osw_file)}..."
-
     _test_measure(osw_file)
-
   end
+  private
 
-
-  def _test_measure(osw_file)
-    require 'json'
-
-    this_dir = File.dirname(__FILE__)
-    osw = File.absolute_path("#{this_dir}/#{osw_file}")
-
+  def _run_osw(model, osw)
     measures = {}
 
     osw_hash = JSON.parse(File.read(osw))
     measures_dir = File.join(File.dirname(__FILE__), osw_hash['measure_paths'][0])
-    puts measures_dir
     osw_hash['steps'].each do |step|
       measures[step['measure_dir_name']] = [step['arguments']]
     end
-
-    model = OpenStudio::Model::Model.new
     runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
 
     # Apply measure
+    cdir = File.expand_path('.')
     success = apply_measures(measures_dir, measures, runner, model)
+    Dir.chdir(cdir) # we need this because of Dir.chdir in HPXMLtoOS
 
     # Report warnings/errors
     runner.result.stepWarnings.each do |s|
@@ -45,5 +36,58 @@ class ResStockArgumentsPostHPXMLTest < Minitest::Test
     end
 
     assert(success)
+  end
+
+  def _upgrade_osw(osw)
+    upgrades = { 'ceiling_assembly_r' => 61.6,
+                 'air_leakage_value' => 2.25 }
+
+    osw_hash = JSON.parse(File.read(osw))
+    osw_hash['steps'].each do |step|
+      step['arguments']['hpxml_path'] = step['arguments']['hpxml_path'].gsub('tests/', 'tests/Upgrade_')
+      if step['measure_dir_name'] == 'BuildResidentialHPXML'
+        step['arguments'].update(upgrades)
+      end
+    end
+    File.open(osw, 'w') { |json| json.write(JSON.pretty_generate(osw_hash)) }
+  end
+
+  def _test_measure(osw_file)
+    require 'json'
+
+    puts "\nTesting #{osw_file}..."
+    this_dir = File.dirname(__FILE__)
+
+    values = { 'hpxml_output' => {} }
+
+    # Existing
+    model = OpenStudio::Model::Model.new
+    osw = File.absolute_path("#{this_dir}/#{osw_file}")
+    _run_osw(model, osw)
+
+    hpxml_path = File.join(this_dir, 'in.xml')
+    hpxml_in = HPXML.new(hpxml_path: hpxml_path)
+
+    existing_path = File.join(this_dir, osw_file.gsub('osw', 'xml'))
+    existing_hpxml = HPXML.new(hpxml_path: existing_path)
+
+    # Upgraded
+    upgrade_osw_file = "Upgrade_#{osw_file}"
+    upgrade_osw = File.absolute_path("#{this_dir}/#{upgrade_osw_file}")
+    FileUtils.cp(osw, upgrade_osw)
+    _upgrade_osw(upgrade_osw)
+    _run_osw(model, upgrade_osw)
+
+    upgraded_path = File.join(this_dir, upgrade_osw_file.gsub('osw', 'xml'))
+    upgraded_hpxml = HPXML.new(hpxml_path: upgraded_path)
+
+    # Create instance of the measures
+    load_flexibility = ResStockArgumentsPostHPXML.new
+
+    # Clean up
+    File.delete(File.join(File.dirname(__FILE__), osw_file.gsub('.osw', '.xml')))
+    File.delete(File.join(File.dirname(__FILE__), upgrade_osw_file))
+    File.delete(File.join(File.dirname(__FILE__), upgrade_osw_file.gsub('.osw', '.xml')))
+    Dir.glob(File.join(File.dirname(__FILE__), 'in.*')).each { |f| File.delete(f) }
   end
 end
