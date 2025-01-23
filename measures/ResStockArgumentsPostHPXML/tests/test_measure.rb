851ba0de1df1b7d75025f1f1c7c59d6fdf15c3c9
Yingli <ylou2@nrel.gov>
2025-01-23 14:49:05 -0700

    test for whole measure

--- measures/ResStockArgumentsPostHPXML/tests/test_measure.rb
+++ measures/ResStockArgumentsPostHPXML/tests/test_measure.rb
@@ -2,15 +2,17 @@
 
 require 'openstudio'
 require_relative '../../../resources/hpxml-measures/HPXMLtoOpenStudio/resources/minitest_helper'
+require_relative '../../../resources/buildstock.rb'
 require_relative '../measure.rb'
 
 class ResStockArgumentsPostHPXMLTest < Minitest::Test
   def test_load_flexibility_measure
-    osw_file = 'SFD_1story_FB_UA_GRG_MSHP_FuelTanklessWH_upgrade.osw'
+    osw_file = 'baseline.osw'
     puts "\nTesting #{File.basename(osw_file)}..."
     _test_measure(osw_file)
   end
   private
+
 
   def _run_osw(model, osw)
     measures = {}
@@ -38,22 +40,27 @@ class ResStockArgumentsPostHPXMLTest < Minitest::Test
     assert(success)
   end
 
-  def _upgrade_osw(osw)
-    upgrades = { 'ceiling_assembly_r' => 61.6,
-                 'air_leakage_value' => 2.25 }
+
+  def _upgrade_osw(osw, loadflex_peak_offset, loadflex_pre_peak_duration_hours, loadflex_pre_peak_offset)
+    upgrades = { 'loadflex_peak_offset' => loadflex_peak_offset,
+                 'loadflex_pre_peak_duration_hours' => loadflex_pre_peak_duration_hours,
+                 'loadflex_pre_peak_offset' => loadflex_pre_peak_offset }
+    puts upgrades
 
     osw_hash = JSON.parse(File.read(osw))
     osw_hash['steps'].each do |step|
       step['arguments']['hpxml_path'] = step['arguments']['hpxml_path'].gsub('tests/', 'tests/Upgrade_')
-      if step['measure_dir_name'] == 'BuildResidentialHPXML'
+      if step['measure_dir_name'] == 'ResStockArgumentsPostHPXML'
         step['arguments'].update(upgrades)
       end
     end
     File.open(osw, 'w') { |json| json.write(JSON.pretty_generate(osw_hash)) }
   end
 
+
   def _test_measure(osw_file)
     require 'json'
+    require 'csv'
 
     puts "\nTesting #{osw_file}..."
     this_dir = File.dirname(__FILE__)
@@ -71,11 +78,16 @@ class ResStockArgumentsPostHPXMLTest < Minitest::Test
     existing_path = File.join(this_dir, osw_file.gsub('osw', 'xml'))
     existing_hpxml = HPXML.new(hpxml_path: existing_path)
 
-    # Upgraded
+    # Upgraded with load flexibility measure
     upgrade_osw_file = "Upgrade_#{osw_file}"
     upgrade_osw = File.absolute_path("#{this_dir}/#{upgrade_osw_file}")
     FileUtils.cp(osw, upgrade_osw)
-    _upgrade_osw(upgrade_osw)
+
+    loadflex_peak_offset = 2
+    loadflex_pre_peak_duration_hours = 2
+    loadflex_pre_peak_offset = 3
+
+    _upgrade_osw(upgrade_osw, loadflex_peak_offset, loadflex_pre_peak_duration_hours, loadflex_pre_peak_offset)
     _run_osw(model, upgrade_osw)
 
     upgraded_path = File.join(this_dir, upgrade_osw_file.gsub('osw', 'xml'))
@@ -84,10 +96,58 @@ class ResStockArgumentsPostHPXMLTest < Minitest::Test
     # Create instance of the measures
     load_flexibility = ResStockArgumentsPostHPXML.new
 
+    # validate setpoint temperature is modified according to measure
+    schedule_file_path = File.join(this_dir, 'in.schedules.csv')
+    # indices for setpint 01-01 14:00:00-15:00:00, 15:00:00-16:00:00, 17:00:00-18:00:00
+    # the on-peak hour in CO in winter starts from 17:00:00
+    heating_indices = [14, 15, 16, 17]
+    # indices for 05-09 13:00:00-14:00:00, 14:00:00-15:00:00, 16:00:00-17:00:00,
+    # the on-peak hour in CO in summer starts from 16:00:00
+    cooling_indices = [3085, 3086, 3087, 3088]
+    heating_rows = []
+    cooling_rows = []
+
+    CSV.foreach(schedule_file_path, headers: true).with_index do |row, index|
+      if heating_indices.include?(index)
+        heating_rows << row.to_h
+      end
+    end
+    CSV.foreach(schedule_file_path, headers: true).with_index do |row, index|
+      if cooling_indices.include?(index)
+        cooling_rows << row.to_h
+      end
+    end
+
+    heating_setpoint_base = celsius_to_fahrenheit(heating_rows[0]["heating_setpoint"].to_f)
+    heating_setpoint_pre_peak = celsius_to_fahrenheit(heating_rows[1]["heating_setpoint"].to_f)
+    heating_setpoint_on_peak = celsius_to_fahrenheit(heating_rows[3]["heating_setpoint"].to_f)
+
+    cooling_setpoint_base = celsius_to_fahrenheit(cooling_rows[0]["cooling_setpoint"].to_f)
+    cooling_setpoint_pre_peak = celsius_to_fahrenheit(cooling_rows[1]["cooling_setpoint"].to_f)
+    cooling_setpoint_on_peak = celsius_to_fahrenheit(cooling_rows[3]["cooling_setpoint"].to_f)
+
+    puts "Testing heating pre peak setpoint offset"
+    assert_equal(loadflex_pre_peak_offset, heating_setpoint_pre_peak - heating_setpoint_base)
+    puts "Testing heating on peak setpoint offset"
+    assert_equal(loadflex_peak_offset, heating_setpoint_base - heating_setpoint_on_peak)
+
+    puts "Testing cooling pre peak setpoint offset"
+    assert_equal(loadflex_pre_peak_offset, cooling_setpoint_base - cooling_setpoint_pre_peak)
+    puts "Testing cooling on peak setpoint offset"
+    assert_equal(loadflex_peak_offset, cooling_setpoint_on_peak - cooling_setpoint_base)
+
     # Clean up
     File.delete(File.join(File.dirname(__FILE__), osw_file.gsub('.osw', '.xml')))
     File.delete(File.join(File.dirname(__FILE__), upgrade_osw_file))
     File.delete(File.join(File.dirname(__FILE__), upgrade_osw_file.gsub('.osw', '.xml')))
     Dir.glob(File.join(File.dirname(__FILE__), 'in.*')).each { |f| File.delete(f) }
+    Dir.glob(File.join(File.dirname(__FILE__), '*.csv')).each { |f| File.delete(f) }
+
   end
+
+  def celsius_to_fahrenheit(celsius)
+    fahrenheit = (celsius * 9.0 / 5.0) + 32
+    return fahrenheit.round
+  end
+  
 end
