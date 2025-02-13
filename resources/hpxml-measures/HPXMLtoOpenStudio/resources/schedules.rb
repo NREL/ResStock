b1e30a01084189605d87b9be95ef425ce563213b
Joe Robertson <joseph.robertson@nrel.gov>
2025-02-13 16:12:49 -0700

    Re-syncing a few hpxml-measures files.

--- resources/hpxml-measures/HPXMLtoOpenStudio/resources/schedules.rb
+++ resources/hpxml-measures/HPXMLtoOpenStudio/resources/schedules.rb
@@ -980,8 +980,6 @@ class SchedulesFile
   # periods CSV (e.g., heating), and/or C) EnergyPlus-specific schedules (e.g., battery_charging).
   Columns = {
     Occupants: Column.new('occupants', true, true, :frac),
-    EVOccupant: Column.new('ev_occupant', true, false, :int),
-    PresentOccupants: Column.new('present_occupants', true, false, :int),
     LightingInterior: Column.new('lighting_interior', true, true, :frac),
     LightingExterior: Column.new('lighting_exterior', true, false, :frac),
     LightingGarage: Column.new('lighting_garage', true, true, :frac),
@@ -1048,7 +1046,6 @@ class SchedulesFile
     return if schedules_paths.empty?
 
     @year = year
-    @runner = runner
     import(schedules_paths)
     create_battery_charging_discharging_schedules
     expand_schedules
@@ -1091,7 +1088,6 @@ class SchedulesFile
   def import(schedules_paths)
     num_hrs_in_year = Calendar.num_hours_in_year(@year)
     @schedules = {}
-    col2path = {}
     schedules_paths.each do |schedules_path|
       # Note: We don't use the CSV library here because it's slow for large files
       columns = File.readlines(schedules_path).map(&:strip).map { |r| r.split(',') }.transpose
@@ -1145,7 +1141,6 @@ class SchedulesFile
         end
 
         @schedules[col_name] = values
-        col2path[col_name] = schedules_path
       end
     end
   end
